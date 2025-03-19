b33b090c5c592713f1d5a7cb9f9a57abc95fee41
ojieunyango <ojieunyango@gmail.com>
2025-03-19 17:35:51 +0900

    feat: 20250319.md student task

--- src/main/java/ch10_setter_getter/Person01.java
+++ src/main/java/ch10_setter_getter/Person01.java
@@ -4,10 +4,8 @@ public class Person01 {
     private String name;
     private int age;
 
-    //기본생성자, 매개변수가 하나인 생성자 두개
-    //매개변수가 두개인 생성자 하나 총네개를 정의 하시오
-
-
+    // 기본 생성자, 매개변수가 하나인 생성자 두 개
+    // 매개변수가 두 개인 생성자 하나 총 네 개를 정의하시오.
     public Person01() {
     }
 
@@ -16,7 +14,14 @@ public class Person01 {
     }
 
     public Person01(int age) {
+        if (age < 0 || age > 200) {
+            System.out.println("불가능한 나이 입력입니다.");
+            return;     // method내에 return; 하면 메서드 그대로 종료
+        }
+        System.out.println("나이가 입력되었습니다.");
+        System.out.println("변경 전 나이 : " + this.age);
         this.age = age;
+        System.out.println("변경 후 나이 : " + age);
     }
 
     public Person01(String name, int age) {
@@ -24,50 +29,55 @@ public class Person01 {
         this.age = age;
     }
 
-    //Setter의 명칭: set + 필드명으로 짓는편
-    //구조: call2() 유형으로 정의됨
+    // Setter의 명칭 : set+필드명으로 짓는 편
+    // 구조 : call2() 유형으로 정의됨.
 
-    public void setName(String name1) {
-        //이름이 글자수가 4 초과라면 입력이 불가능하게끔 로직을 추가하시오
-        //"불가능한 이름입력입니다" 라고 출력되도록하고
-        //이름 바꾸는데 성공했다면
-        //변경전이름:
-        //변경후이름:
-        //이 출력되도록하세요, 여기서 name.length()응용해야합니다
-        //그리고 이거 다끝나셧으면  라고 입력하고 person1.setName("레오나르도 디카프리오")
-        //불가능한 이름 입력입니다를 출력시키시오
-        //그리고 person1.setName()에 여러분 이름을 입력하어0
-        //변경전이름:
-        //변경후이름: 여러분 이름이 나올수있도록 작성하시오
-        //getAge() 메서드도 정의하시오
+    public void setName(String name) {
+        // 이름의 글자 수가 4 초과라면 입력이 불가능 하게끔 로직을 추가하시오.
+        // "불가능한 이름 입력입니다"라고 출력되도록하고,
+        // 이름을 바꾸는데 성공했다면
+        // 변경 전 이름 :
+        // 변경 후 이름 :
+        // 이 출력되도록 하시오. 여기서 name.length()를 응용해야 합니다.
+        // 그리고 이거 다 끝나셨으면 person1.setName("레오나르도디카프리오")라고
+        // 입력하고, 불가능한 이름 입력입니다.를 출력시키시오.
+        // 그리고 person1.setName()에 여러분 이름을 입력하여
+        // 변경 전 이름 : 안근수
+        // 변경 후 이름 : 여러분이름 이 나올 수 있도록 작성하시오.
 
-        if (name.length() > 4) ;
-        System.out.println("불가능한 이름 입력입니다.");
-        return;
-//
-//        System.out.println("방금전이름: "+this.name);
-//        this.name =name;
-//        System.out.println("변경후이름: "+name);
-}
-        public void setAge(int age){
-        //0미만, 200초과의 값이 입력되엇을 경우 "불가능한 나이 입력입니다.
-            //라고 출력하고 값을 바꾸지 않도록 작성할 예정
-            if (age < 0 || age >200){
-                System.out.println("불가능한 나이 입력입니다.");
-                return;// 메서드내에서 리턴하면 메서드 그대로 종료
-            }
-            System.out.println("나이가 입력되었습니다.");
-            System.out.println("방금전나이: "+this.age);
-            this.age =age;
-            System.out.println("변경후나이: "+age);
-
+        // 이거 다 하면 getAge() 메서드도 정의하시오.
+        if(name.length() > 4) {
+            System.out.println("불가능한 이름 입력입니다.");
+            return;
+        }
+        System.out.println("변경 전 이름 : " + this.name);
+        this.name = name;
+        System.out.println("변경 후 이름 : " + this.name);
 
     }
-   //Getter 명칭 : get + 필드명으로 짓는 편
-    //구조: call3() -> 매개변수 없음/ 리턴 잇음
-    //데이터의 조회만하면 되기 때문에 매개변수가 요구되지않음
-    //필드와 동일한 자료형의 리턴값이 요구됨 -> void 아님
-    public String getName(){
+
+    // setAge를 정의하고 PersonMain으로 가서 나이 38을 대입하시오.
+    public void setAge(int age) {
+        // 0 미만, 200초과의 값이 입력됐을 경우 "불가능한 나이 입력입니다"
+        // 라고 출력하고, 값을 바꾸지 않도록 작성할 예정
+        if (age < 0 || age > 200) {
+            System.out.println("불가능한 나이 입력입니다.");
+            return;     // method내에 return; 하면 메서드 그대로 종료
+        }
+        System.out.println("나이가 입력되었습니다.");
+        System.out.println("변경 전 나이 : " + this.age);
+        this.age = age;
+        System.out.println("변경 후 나이 : " + age);
+    }
+
+    // Getter 명칭 : get + 필드명으로 짓는 편
+    // 구조 : call3() -> 매개변수 없음 / return 있음
+    // 데이터의 조회만 하면 되기 때문에 매개 변수가 요구되지 않음.
+    // 필드와 동일한 자료형의 return 값이 요구됨. -> void 아님.
+    public String getName() {
         return name;
+    }
+    public int getAge() {
+        return age;
     }
 }
--- src/main/java/ch10_setter_getter/Person01Main.java
+++ src/main/java/ch10_setter_getter/Person01Main.java
@@ -2,23 +2,28 @@ package ch10_setter_getter;
 
 public class Person01Main {
     public static void main(String[] args) {
-        Person01 person01 = new Person01(-10);
-//        person01.name = "안근수";
-//        System.out.println(person01,name);
-//        System.out.println(person01.age);
-//        person01.age = 38; //private 적용후에 오류발생해서 주석처리
-        //메서드 호출방식 : 객체명.메서드명();
-        person01.setName("안근수");
-        person01.setAge(38);
-        person01.setAge(300);
+        Person01 person1 = new Person01(-10);   // 값은 들어가지 않았지만 객체는 생성됨
+        System.out.println(person1.getName());
+        System.out.println(person1.getAge());
+//        person1.name = "안근수";
+//        System.out.println(person1.name);
+//        System.out.println(person1.age);
+//        person1.age = 38; // private 적용후에 오류 발생해서 주석처리합니다
 
+        //메서드 호출 방식 : 객체명.메서드명();
+        person1.setName("안근수");
+        person1.setAge(38);
+        person1.setAge(300);
 
+        System.out.println(person1.getName());
 
-        System.out.println(person01.getName());
-        person01.setName("레오나르도 디카프리오");
-        System.out.println(person01.getName());
-
-
+        person1.setName("레오나르도디카프리오");
+        System.out.println(person1.getName());
+        person1.setName("김일");
+        System.out.println(person1.getName());
 
     }
 }
+
+
+
