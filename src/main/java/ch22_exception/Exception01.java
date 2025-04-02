78a66a386a78066e780bccd0ad9c5bdbacd08d80
ojieunyango <ojieunyango@gmail.com>
2025-04-02 12:15:12 +0900

    20250402: before changeing

---
+++ src/main/java/ch22_exception/Exception01.java
@@ -0,0 +1,32 @@
+package ch22_exception;
+
+public class Exception01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        try {
+            int result = a / b;
+            System.out.println("결과 : " + result);
+        } catch(ArithmeticException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("프로그램 정상 종료");
+        }
+
+        // 배열 인덱스 예외
+        int[] numbers = {1,2,3};
+
+        try {
+            System.out.println(numbers[5]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("인덱스 범위를 벗어남 : " + e.getMessage());
+        } finally {
+            System.out.println("프로그램 정상 종료");
+        }
+
+
+
+
+    }
+}
+
